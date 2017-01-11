d165bbac08428603c7e79427cdb558e0b501dfe0
Ben <link272@users.noreply.github.com>
2017-01-11 21:30:35 +0100

    Update fuzzy_engine.py

--- fuzzy_engine.py
+++ fuzzy_engine.py
@@ -1,1 +1,29 @@
 import numpy as np
+from gaussien_kde import Gaussien_kde
+
+class FuzzyEngine(object):
+
+	def __init__(self, n_partitions = 3):
+		self.dataset = None
+		self.target = None
+		self.partition = []
+		self.n_partitions = []
+		self.map = []
+		self.hyperarea = []
+
+	def fit(self, x, y):
+		pass
+
+
+	def create_partition():
+		for x in self.dataset:
+			
+		
+
+
+	def create_rules():
+		pass
+
+	def create_hyperarea():
+		pass
+
